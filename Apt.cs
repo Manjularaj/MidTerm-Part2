14d3ab392af909e7a760e9704ac2745793b8b778
Manjularaj <manjularajanap@gmail.com>
2023-03-22 21:52:00 -0700

    Initial commit

---
+++ Apt.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidTerm_Part2
+{
+    public class Apt
+    {
+        string _AptNumber;
+        string _FirstName;
+        string _LastName;
+        decimal _MonthlyPayment;
+        decimal _CurrentBal;
+        float _NumOfBedrooms;
+        string _AptNotes;
+        bool _isOccupied;
+
+       
+
+        public Apt(string name,bool isOccupied = true)
+        {
+            Random rand = new Random();
+
+
+            _FirstName = FirstName;
+            _AptNumber = rand.Next(100, 1000).ToString();
+            _isOccupied = isOccupied;
+
+        }
+
+        //Overloading
+
+        public Apt(string AptNumber, string FirstName)
+        {
+            _AptNumber = AptNumber;
+            _FirstName = FirstName;
+            _isOccupied = true;
+
+        }
+
+        //Constructos dont have return types
+        
+        public Apt(string AptNumber, string FirstName, string LastName, decimal MonthlyPayment, float NumOfBedrooms)
+
+        {
+            _AptNumber = AptNumber;
+            _FirstName = FirstName;
+            _LastName = LastName;
+            _MonthlyPayment = MonthlyPayment;
+            _NumOfBedrooms = NumOfBedrooms;
+
+        }
+
+        public string AptNumber { get => _AptNumber;  }
+        public string FirstName { get => _FirstName; set => _FirstName = value; }
+        public string LastName { get => _LastName; set => _LastName = value; }
+        public decimal MonthlyPayment { get => _MonthlyPayment; set => _MonthlyPayment = value; }
+        public decimal CurrentBal { get => _CurrentBal; set => _CurrentBal = value; }
+        public float NumOfBedrooms { get => _NumOfBedrooms; set => _NumOfBedrooms = value; }
+        public string AptNotes { get => _AptNotes; set => _AptNotes = value; }
+        public bool IsOccupied { get => _isOccupied; set => _isOccupied = value; }
+
+
+        //override to string, to change what display when you call ToString() on an object
+        public override string ToString()
+        {
+            //If is occupied, display Apt number and First name 
+            //else display Apt number and Vacant
+
+            if (IsOccupied) 
+            {
+
+                return $"{_AptNumber} - Firstname{_FirstName}";
+
+            }
+            else
+            {
+
+                return $"{_AptNumber}  - Vacant";
+            }
+                
+           
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MidTerm_Part2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        List<Apt> apartments = new List<Apt>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            Preload();
+            DisplayInformation();
+            DisplayInformation();
+        }
+
+        void Preload()
+        {
+            Random rand = new Random();
+            int end = 106;
+
+
+            for (int i = 100; i < 301; i++)
+            {
+                string apart = "H" + i;
+
+
+                if (rand.Next(2) != 0)
+                {
+                    int monthly = rand.Next(1000, 3000);
+
+                    int bedrooms = rand.Next(1, 4);
+
+                    apartments.Add(new Apt(apart, "Will", "Cram", monthly, bedrooms));
+
+                }
+                else
+                {
+                    apartments.Add(new Apt(apart, false));
+
+                }
+
+
+            }
+
+
+        }
+
+        public void DisplayInformation()
+        {
+            //Clear the Listbox
+            lbTenance.Items.Clear();
+
+            for (int i = 0; i < apartments.Count; i++)
+            {
+
+                lbTenance.Items.Add(apartments[i]);
+            }
+
+        }
+
+        private void btnAddToList_Click(object sender, RoutedEventArgs e)
+        {
+            String userInput = txtFirstName.Text;
+            String luserInput = txtLastName.Text;
+            lbTenance.Items.Add(userInput);
+            //lbTenance.Items.Add(luserInput);
+        }
+
+        private void btnDisplayStudent_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        
+    }
+
+}
